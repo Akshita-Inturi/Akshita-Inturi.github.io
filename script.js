e2b2267163ea5b9ce69a778987bfb4f574d2cc0f
Akshita-Inturi <92865310+Akshita-Inturi@users.noreply.github.com>
2023-06-05 21:55:40 +0530

    added alert

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // DOM manipulation (Document Object Model)
 // document.getElementById
 
+alert("check");
+
 var data1 = {image : "recentPic.jpeg", name : "Akshita Inturi", designation : "Student"};
 var data2 = {image : "childPic.jpeg", name : "Akki Inturi", designation : "stud"};
 
