7cedc35074706a4f6cded1321f401f4a4e7f7585
Akshita-Inturi <92865310+Akshita-Inturi@users.noreply.github.com>
2023-06-05 21:40:57 +0530

    performed first DOM manipulation

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-alert("Daily Progress Tracker Page");
-console.log("Console msg")
+// DOM manipulation (Document Object Model)
+// Document.getElementById
+
+var data1 = {image : "recentPic.jpeg", name : "Akshita Inturi", designation : "Student"};
+var data2 = {image : "childPic.jpeg", name : "Akki Inturi", designation : "Student"};
+
+var isData1 = true;
+var displayData;
+
+var changePic = function() {
+    if(isData1) {
+        displayData = data2;
+        isData1 = false;
+    } else {
+        displayData = data1;
+        isData1 = true;
+        Document.getElementById("myImg").src=displayData.image;
+    }
+}
